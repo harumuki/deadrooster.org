5193c938040e60280b5a1666e037591c08d0e58b
Mickaël Floc’hlay <mickael.flochlay@memo.bank>
2020-10-11 18:15:33 +0200

    fix: reduce size of soundcloud embeds to 166px

--- _plugins/soundcloud_tag.rb
+++ _plugins/soundcloud_tag.rb
@@ -13,7 +13,7 @@ module Jekyll
     def render(_context)
       <<~RENDERED_HTML
         <div>
-          <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay"
+          <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay"
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/#{@track_id}">
           </iframe>
         </div>
